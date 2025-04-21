e4f1923dcb019cdc480864bc75a0ba6afc3402d3
unknown <mdadib300@gmail.com>
2025-04-21 10:21:08 +0600

    Project Completed

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+// ----------------- Long but Basic Process ------------------
 function calculateTriangleArea(){
     // Getting Triangle Base
     const triangleBase = document.getElementById("triangle-base");
@@ -28,3 +29,63 @@ function calculateEllipseArea(){
     const areaSpan = document.getElementById("ellipse-area");
     areaSpan.innerText = area.toFixed(2);
 }
+
+
+
+// --------------------- Precise process with using function
+
+
+// Function to get value from the input
+function getValue(identification){
+    const ValueString = document.getElementById(identification);
+    const Value = parseFloat(ValueString.value);
+    return Value;
+}
+
+// Displaying the area in the UI
+function displayArea(areaIdentification, areaValue){
+    const areaLocation = document.getElementById(areaIdentification);
+    areaLocation.innerText = areaValue.toFixed(2);
+}
+
+
+// Now the work is easier!
+function calculateParallelogramArea(){
+    // getting b
+    const b = getValue("parallelogram-b");
+    // getting h
+    const h = getValue("parallelogram-h");
+    // calculation
+    const area = b*h;
+    // area display
+    displayArea("parallelogram-area", area);
+}
+
+// after removing the comments, see how tiny the code is!
+function calculatePentagonArea(){
+    const p = getValue("pentagon-p");
+    const b = getValue("pentagon-b");
+    const area = 0.5*p*b;
+    displayArea("pentagon-area", area);
+}
+
+function calculateRectangleArea(){
+    const w = getValue("rectangle-w");
+    const l = getValue("rectangle-l");
+    const area = w*l;
+    displayArea("rectangle-area", area);
+}
+
+function calculateRhombusArea(){
+    const d1 = getValue("rhombus-d1");
+    const d2 = getValue("rhombus-d2");
+    const area = 0.5*d1*d2;
+    displayArea("rhombus-area", area);
+}
+
+
+
+
+
+
+// ------------------- PROJECT COMPLETED! - 21/04/2025 ------------------  //
